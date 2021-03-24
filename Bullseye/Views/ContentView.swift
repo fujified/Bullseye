628a0afef2bcc64a1b418dbf87e25a005462fa01
fujified <uktras@gmail.com>
2021-03-24 13:56:44 +0300

    bullseye from scratch

---
+++ Bullseye/Views/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by mr spock on 3/17/21.
+//  Copyright © 2021 mr spock. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var alertIsVisible = false
+    @State private var sliderValue = 50.0
+    @State private var game = Game()
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                    .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                Text(String(game.target))
+                    .kerning(-1.0)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                HStack {
+                    Text("1")
+                        .bold()
+                    Slider(value: $sliderValue, in: 1.0...100.0)
+                    Text("100")
+                        .bold()
+                }
+            }
+            Button(action: {
+                print("hello, swiftui")
+                self.alertIsVisible = true
+            }) {
+                Text("Hit me")
+            }
+            .alert(isPresented: $alertIsVisible, content: {
+                let roundedValue = Int(sliderValue
+                    .rounded())
+                return Alert(title: Text("Hello, There!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome!")))
+            })
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
+    }
+    
+}
---
+++ BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,36 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by mr spock on 3/23/21.
+//  Copyright © 2021 mr spock. All rights reserved.
+//
+
+import XCTest
+@testable import Bullseye
+
+class BullseyeTests: XCTestCase {
+
+    var game: Game!
+    
+    override func setUpWithError() throws {
+        game = Game()
+    }
+
+    override func tearDownWithError() throws {
+        game = nil
+    }
+    
+    func testScorePositive() {
+        let guess = game.target + 5
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+    
+    func testScoreNegative() {
+        let guess = game.target - 5
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+    
+}
